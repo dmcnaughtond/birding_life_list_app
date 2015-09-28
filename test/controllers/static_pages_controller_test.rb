e8c1062247d3db9b1bed81c580324dad879aa235
Daniel McNaughton <d.duncan.mcn@gmail.com>
2015-09-28 14:34:58 -0500

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Birder's Life List App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Birder's Life List App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Birder's Life List App"
+  end
 end
