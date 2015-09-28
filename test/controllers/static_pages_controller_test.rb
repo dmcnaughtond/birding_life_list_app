284cc15c8cfb2db170bebd2025681cb2b2bc2c86
Daniel McNaughton <d.duncan.mcn@gmail.com>
2015-09-28 15:01:27 -0500

    Eliminate repetition (ex 3.1) and add a contact page (ex 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+    def setup
+    @base_title = "Birder's Life List App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Birder's Life List App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Birder's Life List App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Birder's Life List App"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+   test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
